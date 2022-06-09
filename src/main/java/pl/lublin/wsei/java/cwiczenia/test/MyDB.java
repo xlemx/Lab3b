ee24621e55448288eb3db0d5e78a1c57ccbe1000
xlemx <magdalena.lem011@o2.pl>
2022-06-09 16:49:32 +0100

    Ad.5.b.

--- src/main/java/pl/lublin/wsei/java/cwiczenia/test/MyDB.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/MyDB.java
@@ -1,5 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia.test;
-import  java.sql.Connection;
+
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.*;
 import java.util.Properties;
@@ -9,14 +10,24 @@ public class MyDB {
     private Connection conn = null;
     private String User;
     private String Password;
-    public void setUser(String user){User=user;}
-    public void setPassword(String password){Password=password;}
+    String user = "";
+    String password = "";
+    String dbName = "";
+    Integer port = 0;
+    String host = "";
+
+    public MyDB(String localhost, int port, String mydb) {
+    }
+
+    public void setUser(String user) {
+        User = user;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
     private void connect() {
-        String user= "root";
-        String password="425425";
-        String dbName="mydb";
-        String port="3306";
-        String host="localhost";
 
         Properties connectionProps = new Properties();
         connectionProps.put("user", user);
@@ -31,15 +42,24 @@ public class MyDB {
             System.out.println("Błąd podłączenia do bazy:" + jdbcString);
             System.out.println("Komunikat błędu:" + e.getMessage());
             conn = null;
-
         }
         System.out.println("Connected to database" + dbName);
     }
-
 
     public Connection getConnection() {
         if (conn == null)
             connect();
         return conn;
+    }
+
+    public void closeConnection() {
+        if (conn != null)
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("Błąd przy zamykaniu połączenia bazodanowego:" + e.getMessage());
+            }
+        conn = null;
+
     }
 }
---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestMyDB.java
@@ -0,0 +1,18 @@
+package pl.lublin.wsei.java.cwiczenia.test;
+
+import java.sql.Connection;
+
+public class TestMyDB {
+    public static void main(String[] args) {
+
+
+        MyDB db = new MyDB("localhost", 3306, "mydb");
+        db.setUser("root");
+        db.setPassword("*****");
+        Connection conn = db.getConnection();
+        if (conn != null)
+            System.out.println("Połączenie z bazą danych nawiązane");
+        db.closeConnection();
+        System.out.println("Połączeniezbazą zakończone");
+    }
+}
